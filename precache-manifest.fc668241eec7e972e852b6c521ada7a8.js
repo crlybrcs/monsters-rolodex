3b45ba35866c9c3599265c3a30be4f2033fb9fbb
crlybrcs <tatyana_@outlook.com>
2019-12-31 14:24:42 +0100

    Updates

---
+++ precache-manifest.fc668241eec7e972e852b6c521ada7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280ed9a73f867a54b989a72dd9b6ba20",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05ae8877b5368ed6a8e6",
+    "url": "/monsters-rolodex/static/css/main.dc6303eb.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05ae8877b5368ed6a8e6",
+    "url": "/monsters-rolodex/static/js/main.642127fb.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
